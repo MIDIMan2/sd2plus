7766cd0f8b0519e86c45acbdd6d3b1e85f5d07b8
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2026-06-05 21:21:48 +0100

    update scripts in root

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
 del SDOOM2.pk3
 "%ProgramFiles%\7-Zip\7z.exe" a SDOOM2.pk3 -tzip .\dev\SDOOM2\* -r -x!*.pk3 -x!*.bat -x!*.gitignore -x!*.gitattributes -x!"*git\"
-echo All done compiling! Enjoy your fully built Sonic Doom 2: Plus files!
+echo All done compiling! Enjoy your fully built Sonic Doom 2: Plus PK3!
--- testKurejii.bat
+++ testKurejii.bat
@@ -1,7 +1,5 @@
 REM sd2plus testing -- originally by krazeetobi
 cd D:\sd2plus-env
 del SDOOM2.pk3
-del SONIC.pk3
 copy "D:\Git Stuff\sd2plus\SDOOM2.pk3" SDOOM2.pk3
-copy "D:\Git Stuff\sd2plus\SONIC.pk3" SONIC.pk3
-uzdoom -iwad doom2.wad -file SDOOM2.pk3 SONIC.pk3 MAPPY.wad
+uzdoom -iwad doom2.wad -file SDOOM2.pk3 MAPPY.wad
